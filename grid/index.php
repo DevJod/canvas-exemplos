94d4e3a80e3a4ad6c97e573eee621238f7315fe7
Flávio Micheletti <flaviomicheletti@hotmail.com>
2014-05-05 14:07:22 -0300

    grid

---
+++ grid/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <style>
+            body {
+                background: #eeeeee;
+            }
+            canvas {
+                background: #ffffff;
+                cursor: pointer;
+                margin-left: 10px;
+                margin-top: 10px;
+                -webkit-box-shadow: 4px 4px 8px rgba(0,0,0,0.5);
+                -moz-box-shadow: 4px 4px 8px rgba(0,0,0,0.5);
+                box-shadow: 4px 4px 8px rgba(0,0,0,0.5);
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Grid</h1>
+
+        <canvas id='myCanvas' width='300' height='150'>
+            Canvas not supported
+        </canvas>
+        
+        <p><strong>Uma função utilitária para teste visual</strong></p>
+        
+        <!--<p>Matéria explicando o código em <a href="http://localhost/devfuria/www.devfuria.com.br/js/canvas/foo/index.php">www.devfuria.com.br - Linhas randômicas coloridas</a>-->
+        
+        <p><a href="https://github.com/flaviomicheletti">Fork me on git hub</a></p>
+        
+        <script src="index.js"></script>
+    </body>
+</html>
---
+++ grid/readme.md
@@ -0,0 +1,12 @@
+Grid
+===
+
+
+Veja o resultado:
+
+![](https://raw.githubusercontent.com/flaviomicheletti/canvas/master/linhas-rando-coloridas/resultado.png "Linhas coloridas aleatórias")
+
+Matéria explicando o código em [www.devfuria.com.br - Linhas randômicas coloridas](http://www.devfuria.com.br/js/canvas/canvas-linhas-rando-coloridas/) (Live demo)
+
+
+Originalmente desenvolvido por  [David Geary](http://corehtml5canvas.com/code-live/ch02/example-2.13/example.html)
