d72786e17d00c1366e2a3de983f4fdc9c3a3fdc8
Flávio Micheletti <flaviomicheletti@hotmail.com>
2014-05-05 14:38:29 -0300

    update grid/index.js

--- grid/index.js
+++ grid/index.js
@@ -6,12 +6,14 @@ function drawGrid(context, color, stepx, stepy) {
     context.strokeStyle = color;
     context.lineWidth = 0.5;
 
+    //  linhas verticais
     for (var i = stepx + 0.5; i < context.canvas.width; i += stepx) {
         context.moveTo(i, 0);
         context.lineTo(i, context.canvas.height);
         context.stroke();
     }
 
+    //  linhas horizontais
     for (var i = stepy + 0.5; i < context.canvas.height; i += stepy) {
         context.moveTo(0, i);
         context.lineTo(context.canvas.width, i);
